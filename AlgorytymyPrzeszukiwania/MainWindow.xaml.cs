5c6f8b63450af7a4a5a2528bf5b8a12b31db6bd3
Sebastian Chojnacki <Sebastian.Chojnacki@365.aplitt.pl>
2022-06-09 20:16:40 +0200

    Add project files.

---
+++ AlgorytymyPrzeszukiwania/MainWindow.xaml.cs
@@ -0,0 +1,329 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AlgorytymyPrzeszukiwania
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        int Hmany = 0;
+        int Irepeat = 1; 
+        string SSsearch;
+        string dane;
+        string[] lines;
+
+        private void Btn_load_Click(object sender, RoutedEventArgs e)
+        {
+            dane = string.Empty;
+            Debug.WriteLine("102 plus " + dane);
+            stringholder.ItemsSource = "";
+            TabControl_Main.IsEnabled = true;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                using (var streamReader = File.OpenText(openFileDialog.FileName))
+                {
+                    dane = System.IO.File.ReadAllText(openFileDialog.FileName);
+                     lines= System.IO.File.ReadAllLines(openFileDialog.FileName);
+
+                }
+                stringholder.ItemsSource = dane;
+               
+                
+
+
+            }
+            Debug.WriteLine(dane);
+            MessageBox.Show("Długość twojego napisu:" + dane.Length+"To twoj napis:" + dane);
+          
+            
+        }
+
+      
+        private void repeatInput_previewtextinput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+        private void repeatInput_PreviewTextInput_1(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+        private void btn_repeat_Click(object sender, RoutedEventArgs e)
+        {
+            Irepeat = 1;
+           
+            
+            Irepeat = int.Parse(repeatInput.Text);
+            repeatInput.Text = "";
+            repeatInput.Text = "" + Irepeat;
+            MessageBox.Show("ilość twoich powtorzen: " + Irepeat);
+        }
+
+        private void btn_start_bf_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            if (String.IsNullOrEmpty(SearchInput.Text))
+            {
+                MessageBox.Show("Uzupełnij pole!!!");
+            }
+            else
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                SSsearch = SearchInput.Text;
+            
+
+                stopwatch.Start();
+                int SearchLong = SSsearch.Length;
+                int DaneLong = dane.Length;
+
+                char[]  SearchChar= SSsearch.ToCharArray();
+                char[] daneChar= dane.ToCharArray();
+
+                int textIndex = 0;
+                
+
+
+               
+               
+                for (int k = 0; k < Irepeat; k++)
+                {
+                    Debug.WriteLine("start");
+                    Hmany = 0;
+
+                    for (textIndex = 0; textIndex < DaneLong; textIndex++)
+                    {
+                       
+                        int textIndexLocal = textIndex;
+                        Boolean match = true;
+                        int matchedIndex = textIndex;
+                        for (int patternIndex = 0; patternIndex < SearchLong; patternIndex++)
+                        {
+                            if (daneChar[textIndexLocal] != SearchChar[patternIndex])
+                            {
+                                match = false;
+                                break;
+                                Debug.WriteLine("brake");
+                            }
+                            textIndexLocal = textIndexLocal + 1;
+                        }
+                        if (match)
+                        {
+                            Hmany++;
+                        }
+                    }
+                    Debug.WriteLine("koniec");
+                }
+
+
+
+
+
+
+
+                stopwatch.Stop();
+                
+                MessageBox.Show("znalezniono " + Hmany);
+
+
+                TEXT_time_BF.Text = "" + stopwatch.ElapsedMilliseconds;
+            }
+
+
+      
+        }
+
+        private void btn_start_kmp_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            if (String.IsNullOrEmpty(SearchInput_KMP.Text))
+            {
+                MessageBox.Show("Uzupełnij pole!!!");
+            }
+            else
+            {
+                SSsearch = SearchInput.Text;
+                Stopwatch stopwatch = new Stopwatch();
+
+                stopwatch.Start();
+
+
+                for (int k = 0; k < Irepeat; k++)
+                {
+                    
+                }
+
+
+
+
+
+
+
+
+                stopwatch.Stop();
+
+                
+                TEXT_time_KMF.Text = "" + stopwatch.ElapsedMilliseconds;
+
+            }
+        }
+
+
+        private void btn_start_BM_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+
+            int bmResult = 0 ;
+            
+
+            if (String.IsNullOrEmpty(SearchInput_BM.Text))
+            {
+                MessageBox.Show("Uzupełnij pole!!!");
+            }
+            else
+            {
+                 static int[] BuildBadCharTable2(char[] needle)
+                {
+                    int[] badShift = new int[256];
+                    for (int i = 0; i < 256; i++)
+                    {
+                        badShift[i] = needle.Length;
+                    }
+                    int last = needle.Length - 1;
+                    for (int i = 0; i < last; i++)
+                    {
+                        badShift[(int)needle[i]] = last - i;
+                    }
+                    return badShift;
+                }
+
+
+
+                 static int BoyerMoore(String pattern, String text)
+                {
+                    char[] needle = pattern.ToCharArray();
+                    char[] haystack = text.ToCharArray();
+
+                    if (needle.Length > haystack.Length)
+                    {
+                        return -1;
+                    }
+                    int[] badShift = BuildBadCharTable2(needle);
+                    int offset = 0;
+                    int scan = 0;
+                    int last = needle.Length - 1;
+                    int maxoffset = haystack.Length - needle.Length;
+                    while (offset <= maxoffset)
+                    {
+                        for (scan = last; (needle[scan] == haystack[scan + offset]); scan--)
+                        {
+                            if (scan == 0)
+                            { 
+                                return offset;
+                            }
+                        }
+                        offset += badShift[(int)haystack[offset + last]];
+                    }
+                    return -1;
+                }
+
+
+
+
+               
+
+
+                Stopwatch stopwatch = new Stopwatch();
+
+
+
+                    for (int k = 0; k < Irepeat; k++)
+                {
+               
+
+                    stopwatch.Start();
+                    char[] SearchChar = SSsearch.ToCharArray();
+                    char[] daneChar = dane.ToCharArray();
+                    bmResult = BoyerMoore(SSsearch, dane);
+                   
+
+
+
+                }
+                    stopwatch.Stop();
+
+                TEXT_time_BM.Text = "" + stopwatch.ElapsedMilliseconds;
+                MessageBox.Show("znalezniono " + bmResult);
+
+
+            }
+           
+
+        }
+
+
+        private void btn_start_RK_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+            Hmany = 0;
+            if (String.IsNullOrEmpty(SearchInput_RK.Text))
+            {
+                MessageBox.Show("Uzupełnij pole!!!");
+            }
+            else
+            {
+
+
+
+
+                Hmany = 0;
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+
+                for (int k = 0; k < Irepeat; k++)
+                {
+
+                }
+                    stopwatch.Stop();
+
+                TEXT_time_RK.Text = "" + stopwatch.ElapsedMilliseconds;
+                MessageBox.Show("znalenizono:" + Hmany + " razy");
+
+            }
+
+
+
+
+        }
+
+        
+    }
+}
